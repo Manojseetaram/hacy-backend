f497308744b7dff2fc20f654fe4d614e41cd68ec
sujaykumar007 <sujayadur551@gmail.com>
2024-11-16 12:30:19 +0530

    sigin feature added

---
+++ controllers/signin.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+
+const signIn = async (req,res) => {
+    const { email,password } = req.body;
+    if (!email ||  !password) {
+        return res.status(400).json({ message: 'Please fill all fields' });
+    }
+
+    try {
+
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+          return res.status(400).json({ message: 'User with this email not exists' });
+        }
+
+        const isPasswordMatching = await bcrypt.compare(password,existingUser.password)
+        
+        if(!isPasswordMatching) {
+            return res.status(401).json({message:"incorrect password"});
+        }
+
+        return res.status(200).json({message:"login successfull"});
+
+    }catch(err) {
+        console.log(err)
+        return res.status(500).json({message:"server errorr"});
+    }
+
+}
+
+module.exports=signIn;
